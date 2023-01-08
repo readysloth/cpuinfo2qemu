ef53d6ce71c324073f6028484972a7241d9f4177
readysloth <readysloth@protonmail.com>
2023-01-08 21:49:13 +0300

    feat(cpuinfo2qemu.py): Supported input from different source

--- cpuinfo2qemu.py
+++ cpuinfo2qemu.py
@@ -12,11 +12,18 @@ def get_flags_from_cpuinfo(lines):
     return flags
 
 
-def read_cpuinfo():
-    with open('/proc/cpuinfo', 'r') as f:
-        lines = [l.strip() for l in f.readlines()]
+def read_cpuinfo(filename='/proc/cpuinfo'):
+    if filename == '-':
+        lines = sys.stdin.readlines()
+    else:
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+    lines = [l.strip() for l in lines]
+    try:
         first_blank_line = lines.index('')
-        return lines[:first_blank_line]
+    except ValueError:
+        return lines
+    return lines[:first_blank_line]
 
 
 def get_flags_from_qemu(qemu):
@@ -34,10 +41,11 @@ parser.add_argument('qemu',
                     nargs='?',
                     default='qemu-system-x86_64',
                     help='qemu executable, e.g. qemu-system-x86_64')
+parser.add_argument('-f', '--file', help='use file instead of /proc/cpuinfo')
 parser.add_argument('-d', '--debug', action='store_true', help='debug output')
 args = parser.parse_args()
 
-cpu_supported_flags = get_flags_from_cpuinfo(read_cpuinfo())
+cpu_supported_flags = get_flags_from_cpuinfo(read_cpuinfo(args.file if args.file else '/proc/cpuinfo'))
 qemu_supported_flags = get_flags_from_qemu(args.qemu)
 
 mutually_supported = set(qemu_supported_flags).intersection(cpu_supported_flags)
